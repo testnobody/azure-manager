85dfe81ce54675c0553c8715c06cac80c5d7427d
testnobody <70645370+testnobody@users.noreply.github.com>
2021-07-30 17:52:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -143,4 +143,5 @@ def changeip_vm(tag):
 
 
 if __name__ == '__main__':
-    app.run(port=23333, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port, host='0.0.0.0')
