6e1852bfac6bdda97e40e58d0323ef9d169c9b65
kyohan789 <56071904+kyohan789@users.noreply.github.com>
2021-08-02 19:42:51 +0800

    Update function.py

--- function.py
+++ function.py
@@ -143,7 +143,7 @@ def create_or_update_vm(subscription_id, credential, tag, location, username, pa
                                                                   "storage_profile": {
                                                                       "osDisk":{
                                                                           "createOption":"fromImage",
-                                                                          "diskSizeGB":30
+                                                                          "diskSizeGB":64
                                                                       },
                                                                       "image_reference": {
                                                                           "offer": offer,
